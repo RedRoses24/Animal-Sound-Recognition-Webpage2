ea6b401578e1237a0d83da758051b00528da6a94
RedRoses24 <87375785+RedRoses24@users.noreply.github.com>
2022-04-04 18:50:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function start_classification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/dsMBrOiTk/.model.json', modelReady)
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+    console.log('Got results');
+}
